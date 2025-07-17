05e987c7a6574dd510ab1d022e31b1abcebd2ec7
Ladytrond <gorda54@hotmail.com>
2025-07-16 22:55:53 -0300

    Update PlanView.jsx

--- PrePagaSalud-Web/src/views/PlanView.jsx
+++ PrePagaSalud-Web/src/views/PlanView.jsx
@@ -6,6 +6,7 @@ import { Container, Row, Col } from "react-bootstrap"
 
 function PlanView(){
     const host = import.meta.env.VITE_API_URL;
+    const baseUrl = import.meta.env.VITE_BASE_URL;
     const {id} = useParams();
     const [plan, setPlan] = useState({
         nombre: "", 
@@ -55,6 +56,32 @@ function PlanView(){
                             prepaga={plan.prepaga}
                             tarifa={plan.tarifa}
                         />
+
+                        <Card className="mb-4">
+                            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                            <Card.Body>
+                                <div className="d-flex mb-2 justify-content-between align-items-center">
+                                    <Card.Title className="pe-2 mb-0">{plan.nombre}</Card.Title>
+                                    {plan.prepaga.logo &&
+                                        <img src={`${baseUrl}/${plan.prepaga?.logo}`} width="auto" height={32} alt={plan.prepaga?.nombre}/>
+                                    }
+                                </div>
+                                <ul className="list-unstyled">
+                                    <li><small className="text-secondary">Prepaga: </small><span className="h6">{plan.prepaga?.nombre}</span></li>
+                                    <li><small className="text-secondary">Edad:</small> {plan.rangoEtario.min} a {plan.rangoEtario.max} años.</li>
+                                    <li><small className="text-secondary">Grupo Familiar:</small> {plan.grupoFamiliar.join(', ')}</li>
+                                </ul>
+                                {plan.cobertura &&
+                                    <p><strong>Cobertura: </strong><br/>{plan.cobertura}</p>
+                                }
+                                <p className="h6 fw-normal"><strong>Tarifa:</strong> $ {plan.tarifa}</p>
+                            </Card.Body>
+                            <Card.Footer>
+                                <div className="d-flex">
+                                    <Button variant="primary" disabled size="sm">Consultar</Button>
+                                </div>
+                            </Card.Footer>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
